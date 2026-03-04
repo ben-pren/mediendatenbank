e20127b7117ef0ae131c9c6c535982e531b86db8
deltadh <115051902+deltadh@users.noreply.github.com>
2026-03-04 19:50:31 +0100

    Erster Entwurf der Startseite und CSS

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mediendatenbank</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<?php include 'header.php'; ?>
+	</header>
+	<section>
+		<h1>H1</h1>
+		<h2>H2</h2>
+		<h3>H3</h3>
+		<p>P</p>
+		<p class="small">P(klein)</p>
+	</section>
+	<footer>
+		<p>Gruppenarbeit WEB42</p>
+	</footer>
+</body>
+</html>
