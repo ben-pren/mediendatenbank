bf214ebeab27c14e4aa3868315fe077116cc85ff
Greta Guntermann <greta.guntermann@stud.uni-goettingen.de>
2026-03-11 23:21:03 +0100

    Erstellung Landing Page, Updates Anforderungen

--- public/index.php
+++ public/index.php
@@ -6,6 +6,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Mediendatenbank</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<style>
+        section {
+        	text-align: center;
+    		padding-top: 90px;
+        	padding-bottom: 90px;
+   	 	}
+    	.foto-stil {
+        	width: 180px;
+        	height: 240px;
+        	margin: 17px; /* abstand zwischen bildern */
+        	object-fit: cover; /*passt format an, damit keine verzerrungen entstehen*/
+   	 	}
+    </style>
 </head>
 <body>
 	<header>
@@ -13,11 +27,19 @@
 	</header>
 	
 	<section>
-		<h1>H1</h1>
-		<h2>H2</h2>
-		<h3>H3</h3>
-		<p>P</p>
-		<p class="small">P(klein)</p>
+		<h1>Willkommen im MedienHub!</h1>
+		<!-- <h2>H2</h2> -->
+		<h3></h3>
+		<h3>Hier können Sie Medien hochladen, ansehen und herunterladen.</h3>
+		<p>Schnell, einfach und kostenlos.</p>
+
+		<div class="foto-container">
+        	<img src="icons/bild1_landingpage.jpg" class="foto-stil" alt="Vorschau 1: Buch">
+        	<img src="icons/bild2_landingpage.jpg" class="foto-stil" alt="Vorschau 2: New York">
+        	<img src="icons/bild3_landingpage.jpg" class="foto-stil" alt="Vorschau 3: Dackel">
+		</div>
+		
+		<!-- <p class="small">P(klein)</p>-->
 	</section>
 
 	<footer>
