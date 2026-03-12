310b4dcd278cc5437f553a16e6e6039dbe0ddd14
Robin-brtz <bretzkerobin@gmail.com>
2026-03-12 23:01:07 +0100

    Index.php Video fix + Hintergrund

--- public/index.php
+++ public/index.php
@@ -25,21 +25,23 @@
 	<header>
 		<?php include  __DIR__ . '/../src/includes/header.php'; ?>
 	</header>
-	
+		<?php include __DIR__ . '/../src/includes/background.php'; ?>
 	<section>
 		<h1>Willkommen im MedienHub!</h1>
 		<!-- <h2>H2</h2> -->
 		<h3></h3>
-		<h3>Hier können Sie Medien hochladen, ansehen und herunterladen.</h3>
+		<h3>Hier kannst du Medien hochladen, ansehen und herunterladen.</h3>
 		<p>Schnell, einfach und kostenlos.</p>
 
 		<div class="foto-container">
         	<img src="icons/bild1_landingpage.jpg" class="foto-stil" alt="Vorschau 1: Buch">
         	<!--  <img src="icons/bild2_landingpage.jpg" class="foto-stil" alt="Vorschau 2: New York"> -->
-			<video src="icons/video1_landingpage.mov" class="foto-stil" alt="Vorschau 2: London" autoplay loop></video>
+			<video src="icons/video1_landingpage.mp4" class="foto-stil" autoplay loop muted></video>
         	<img src="icons/bild3_landingpage.jpg" class="foto-stil" alt="Vorschau 3: Dackel">
 		</div>
-		<p>Wir unterstützen: Fotos, Videos, Hörbücher & eBooks!</p>
+		<p>Wir unterstützen: Fotos, Videos, Hörbücher &amp; eBooks!</p> <br>
+		<p>Schau in der Galerie vorbei, um zu sehen, was andere Nutzer bereits hochgeladen haben, <br>
+		   und melde dich an, um selbst etwas hochzuladen</p>
 
 		<!-- <p class="small">P(klein)</p>-->
 	</section>
