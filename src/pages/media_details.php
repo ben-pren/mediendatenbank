3b2a09e4143bc05e68ce588fe1271a81bd27d9ac
Robin-brtz <bretzkerobin@gmail.com>
2026-04-03 14:56:54 +0200

    Update media_details.php

--- src/pages/media_details.php
+++ src/pages/media_details.php
@@ -113,7 +113,7 @@ $modus = (isset($_POST['bearbeiten']) || isset($_GET['bearbeiten'])) ? 'edit' : 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mediendatenbank</title>
+    <title>Mediendetails</title>
     <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
 </head>
 <body>
